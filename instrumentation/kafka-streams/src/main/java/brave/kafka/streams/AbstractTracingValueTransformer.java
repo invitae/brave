aaa4998371cb9804cdfdf0dbe97e7de498d26563
alexk <alexk@invitae.com>
2019-06-28 02:06:23 +0200

    moar liberty

--- instrumentation/kafka-streams/src/main/java/brave/kafka/streams/AbstractTracingValueTransformer.java
+++ instrumentation/kafka-streams/src/main/java/brave/kafka/streams/AbstractTracingValueTransformer.java
@@ -16,7 +16,7 @@ package brave.kafka.streams;
 import org.apache.kafka.streams.kstream.ValueTransformer;
 import org.apache.kafka.streams.processor.ProcessorContext;
 
-abstract class AbstractTracingValueTransformer<V, VR> implements
+public abstract class AbstractTracingValueTransformer<V, VR> implements
     ValueTransformer<V, VR> {
 
   @Override public void init(ProcessorContext context) {
